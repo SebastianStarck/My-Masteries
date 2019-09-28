49fd4bdce95cccfa89ae93709b8cd1efa861aafe
Sebastián <sebastian@starck.im>
2019-09-28 03:45:16 -0300

    Added module: Cache

---
+++ My Masteries/src/ts/Cache.ts
@@ -0,0 +1,20 @@
+﻿import NodeCache = require('node-cache');
+import { Summoner, Champion } from './Interfaces';
+export const cache = new NodeCache();
+const cacheDurationInDays = 7;
+
+export function getSummonerFromCache(summonerName: string, region: string): Summoner | undefined {
+    return cache.get(`${region}_${summonerName}`);
+}
+
+export function cacheSummoner(summoner: Summoner): boolean {
+    return cache.set(`${summoner.region}_${summoner.name}`, summoner, cacheDurationInDays * 86400);
+}
+
+export function getChampionsDataFromCache(): Map<string, Champion> | undefined {
+    return cache.get('mappedChampions');
+}
+
+export function cacheChampionsData(championsData: Map<string, Champion>): boolean {
+    return cache.set('mappedChampions', championsData, cacheDurationInDays * 86400);
+}
