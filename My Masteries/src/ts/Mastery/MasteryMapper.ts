2507bca9da2e938753c632691ebf3795cb4bd70e
Sebastián <sebastian@starck.im>
2019-10-01 23:57:53 -0300

    Broke down MasteryDigester, finished MasteryProfile

---
+++ My Masteries/src/ts/Mastery/MasteryMapper.ts
@@ -0,0 +1,62 @@
+﻿import { IChampionMastery } from "../Interfaces";
+import Cache = require('../Cache');
+import { MasteriesProfile } from "./MasteryProfile";
+import { Champion } from "../Champion/Champion";
+import { ChampionsDataNotFound } from "../Exceptions/Exceptions";
+
+export async function mapMasteries(masteries: Array<IChampionMastery>) {
+    const champions = await Cache.getChampions();
+    const masteriesProfile = new MasteriesProfile(masteries);
+
+    masteries.forEach((championMastery) => {
+        const champion: Champion = champions[`${championMastery.championId}`];
+        try {
+            mapSimpleAttributes(champion, championMastery, masteriesProfile);
+            mapComplexAttributes(champion, masteriesProfile, champions, championMastery);
+        } catch (e) {
+            throw new ChampionsDataNotFound(`Champion ${champion.id} attributes not found`);
+        }
+    });
+
+    return masteriesProfile;
+}
+
+function mapComplexAttributes(champion: object, masteriesProfile: MasteriesProfile, champions: {}, championMastery: IChampionMastery) {
+    ['info', 'advancedStats'].forEach((attribute) => {
+        const championAttributeKeys = Object.keys(champion[attribute]);
+        championAttributeKeys.forEach((subAttribute) => {
+            if (subAttribute != 'damageRating') {
+                mapSubAttribute(attribute, subAttribute);
+            }
+            else {
+                mapDamageRating(attribute, subAttribute);
+            }
+        });
+    });
+
+    function mapDamageRating(attribute: string, subAttribute: string) {
+        const championDamageRating = champion[attribute][subAttribute];
+        const damageRating = masteriesProfile.advancedStats.get(subAttribute);
+
+        damageRating[championDamageRating] = damageRating[championDamageRating] + 1 || 1;
+    }
+
+    function mapSubAttribute(attribute: string, subAttribute: string) {
+        const currentStatScore = masteriesProfile[attribute].get(subAttribute) || 0;
+        const subAttributeValue = champions[championMastery.championId][attribute][subAttribute] || 0;
+        const masteryCoefficient = (championMastery.championPoints / masteriesProfile.totalMasteryPoints) * subAttributeValue;
+
+        masteriesProfile[attribute].set(`${subAttribute}`, currentStatScore + masteryCoefficient);
+    }
+}
+
+function mapSimpleAttributes(champion: object, championMastery: IChampionMastery, masteriesProfile: MasteriesProfile) {
+    ['tags', 'advancedTags', 'lanes'].forEach((attribute) => {
+        champion[attribute].forEach((subAttribute) => {
+            const subAttributeName = `${subAttribute.toLowerCase()}MasteryPoints`;
+            const subAttributeScore = championMastery.championPoints + (masteriesProfile[attribute].get(subAttributeName) || 0);
+
+            masteriesProfile[attribute].set(`${subAttributeName}`, subAttributeScore);
+        });
+    });
+}
---
+++ My Masteries/src/ts/Mastery/MasteryProfile.ts
@@ -0,0 +1,64 @@
+﻿import ChampionData = require('../Champion/ChampionData');
+import { IChampionMastery } from '../Interfaces';
+import { InvalidMasteryProfileAttributeMap } from '../Exceptions/Exceptions';
+
+export class MasteriesProfile {
+    totalMasteryPoints: number;
+    info: Map<string, number>;
+    advancedStats: Map<string, number | object>;
+    tags: Map<string, number>;
+    advancedTags: Map<string, number>;
+    lanes: Map<string, number>;
+    masteries: Array<IChampionMastery>
+
+    constructor(masteries: Array<IChampionMastery>) {
+        this.info = new Map();
+        this.advancedStats = new Map();
+        this.tags = new Map();
+        this.lanes = new Map();
+        this.advancedTags = new Map();
+        this.advancedStats.set('damageRating', {});
+        this.masteries = masteries;
+        this.totalMasteryPoints = addUpMasteriesPoints(masteries);
+    };
+
+    public getAttributeByContribution = (attribute: string): Map<string, number> => {
+        const map = new Map();
+
+        const attributeMap: Map<string, number> = this.getAttributeMap(attribute);
+        const attributeMasteryPoints: number = Array.from(attributeMap.values())
+            .reduce((total: number, mastery: number) => total + mastery, 0);
+
+        attributeMap.forEach((subAttributeValue: number, subAttributeKey: string) => {
+            map.set(subAttributeKey, (subAttributeValue * 100) / attributeMasteryPoints);
+        });
+
+        return map;
+    };
+
+    private getAttributeMap = (attribute: string): Map<string, number> => {
+        const attributeMap = this[attribute];
+
+        if (!(attributeMap instanceof Map) || !attributeMap) {
+            throw new InvalidMasteryProfileAttributeMap();
+        }
+
+        return attributeMap;
+    }
+
+    public getTopChampions(amount?: number): Array<string> {
+        return this.masteries.slice(0, amount || 5).map((championMastery) => {
+            return ChampionData.getChampionName(championMastery.championId);
+        });
+    };
+
+    public getMasteriesProfileArchtype = () => {
+
+    };
+}
+
+function addUpMasteriesPoints(masteries: Array<IChampionMastery>): number {
+    return masteries
+        .map((championMastery) => { return championMastery.championPoints })
+        .reduce((accumulator, currentValue) => accumulator + currentValue);
+}
